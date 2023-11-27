30ec3ed61dd7ff2b8e288a9f1806daeb9224c538
useonglee <useong0830@naver.com>
2023-11-27 22:59:57 +0900

    fix: services 폴더 내부 프로젝트만 타입 검사하도록 수정

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
   '*.{ts,tsx,js,jsx}': () => 'yarn eslint --fix',
-  '**/*.(ts|tsx)': () => 'yarn tsc --noEmit -p tsconfig.json',
+  'services/src/**/*.(ts|tsx)': () => 'yarn tsc --noEmit -p tsconfig.json',
 };
