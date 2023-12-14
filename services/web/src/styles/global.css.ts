5b080001db9e765a0cf289d2058b52410886a6f0
JungminLee <85864699+JjungminLee@users.noreply.github.com>
2023-12-14 15:43:04 +0900

    feat: [LINKER-23] vanilla extract global style 설정 (#8)
    Co-authored-by: Useong Lee <useong0830@naver.com>

---
+++ services/web/src/styles/global.css.ts
@@ -0,0 +1,38 @@
+import { globalStyle } from '@vanilla-extract/css';
+
+export const GlobalStyle = [
+  globalStyle('html', {
+    fontSize: '62.5%',
+    margin: 0,
+    padding: 0,
+    overflowX: 'hidden',
+    height: '100%',
+  }),
+  globalStyle('body', {
+    margin: 0,
+    padding: 0,
+    overflowX: 'hidden',
+    fontSize: '1.6rem',
+  }),
+  globalStyle('*, *:before, *:after', {
+    boxSizing: 'border-box',
+  }),
+  globalStyle('focus', {
+    outline: 'none',
+    border: 'none',
+  }),
+  globalStyle('button', {
+    background: 'none',
+    padding: 0,
+    border: 'none',
+    cursor: 'pointer',
+  }),
+  globalStyle('a', {
+    color: 'inherit',
+    textDecoration: 'none',
+    cursor: 'pointer',
+  }),
+  globalStyle('table', { borderCollapse: 'collapse', borderSpacing: 0 }),
+
+  globalStyle('img, picture, video, canvas, svg ', { display: 'block', maxWidth: '100%' }),
+];
