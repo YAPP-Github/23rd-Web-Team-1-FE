7e473c916aae66caed945a4c30d7294d55941384
Useong Lee <useong0830@naver.com>
2023-12-17 21:36:37 +0900

    feat: [LINKER-35] normalize css 추가 (#12)

--- services/web/src/app/layout.tsx
+++ services/web/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import '../../src/styles/global.css';
+
+import '../styles/global.css';
+import '../styles/normalize.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
