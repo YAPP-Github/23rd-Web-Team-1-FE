c59c76a98daa53108594798ea3c498a6a3c21888
danivelop <mofg266@gmail.com>
2023-12-17 21:51:04 +0900

    feat: 연락처, 타임라인 route page 생성

---
+++ services/web/src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+function ContactPage() {
+  return (
+    <div>
+      <h1>연락처</h1>
+    </div>
+  );
+}
+
+export default ContactPage;
---
+++ services/web/src/app/timeline/page.tsx
@@ -0,0 +1,9 @@
+function TimelinePage() {
+  return (
+    <div>
+      <h1>타임라인</h1>
+    </div>
+  );
+}
+
+export default TimelinePage;
