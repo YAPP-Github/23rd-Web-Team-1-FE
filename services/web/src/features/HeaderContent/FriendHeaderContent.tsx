2deea68446053a40e9e244f5f18cd4fb77a70710
useonglee <useong0830@naver.com>
2023-12-25 00:15:43 +0900

    fix: Tabs 컴포넌트 path alias 수정

--- services/web/src/features/HeaderContent/FriendHeaderContent.tsx
+++ services/web/src/features/HeaderContent/FriendHeaderContent.tsx
@@ -1,36 +1,36 @@
-'use client'
+'use client';
 
-import { Tabs, TabInfo } from '@linker/lds/src/Tabs'
-import { clsx } from 'clsx'
-import { useParams } from 'next/navigation'
-import { useRef } from 'react'
+import { Tabs, TabInfo } from '@linker/lds';
+import { clsx } from 'clsx';
+import { useParams } from 'next/navigation';
+import { useRef } from 'react';
 
-import { contentWrapper, fixedHeader } from './FriendHeaderContent.css'
-import { FriendProfile } from './components/FriendProfile'
-import useMinimize from './hooks/useMinimize'
+import { contentWrapper, fixedHeader } from './FriendHeaderContent.css';
+import { FriendProfile } from './components/FriendProfile';
+import useMinimize from './hooks/useMinimize';
 
-const MINIMIZE_THRESHOLD = 150
-const MAXIMIZE_THRESHOLD = 30
+const MINIMIZE_THRESHOLD = 150;
+const MAXIMIZE_THRESHOLD = 30;
 
 function FriendHeaderContent() {
-  const { id } = useParams()
+  const { id } = useParams();
 
   const tabInfos = useRef<TabInfo[]>([
     { href: `/friend/${id}/subject`, text: '관심주제', textSpan: null },
     { href: `/friend/${id}/timeline`, text: '타임라인', textSpan: null },
-  ])
+  ]);
 
   const isMinimize = useMinimize({
     minimizeThreshold: MINIMIZE_THRESHOLD,
     maximizeThreshold: MAXIMIZE_THRESHOLD,
-  })
+  });
 
   return (
     <div className={clsx(contentWrapper, isMinimize && fixedHeader)}>
       <FriendProfile isMinimize={isMinimize} />
       <Tabs tabInfos={tabInfos} />
     </div>
-  )
+  );
 }
 
-export default FriendHeaderContent
+export default FriendHeaderContent;
