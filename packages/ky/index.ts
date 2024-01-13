ddc4533d5d1f74ecaa5427f94e407e238bfd3b72
useonglee <useong0830@naver.com>
2024-01-14 02:23:57 +0900

    feat: ky 인터페이스 수정

--- packages/ky/index.ts
+++ packages/ky/index.ts
@@ -1,15 +1,3 @@
-import ky from 'ky';
+import kyInstance, { createKyApis } from './kyInstance';
 
-const kyInstance = ky.create({
-  retry: 0,
-  credentials: 'include',
-  hooks: {
-    beforeRequest: [
-      (request) => {
-        // TODO(@useonglee): 토큰 로직 추가하기
-      },
-    ],
-  },
-});
-
-export default kyInstance;
+export const ky = createKyApis(kyInstance);
