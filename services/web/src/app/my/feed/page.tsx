c5be73dc0f57ec52c5872a7018c21e73165ac2bb
danivelop <mofg266@gmail.com>
2024-01-20 02:09:06 +0900

    chore: eslint와 prettier간 설정 충돌로 인해 prettier가 적용 안되는 문제 수정

--- services/web/src/app/my/feed/page.tsx
+++ services/web/src/app/my/feed/page.tsx
@@ -1,8 +1,8 @@
 import { ContactBanner } from './contact-banner';
-import { wrapper } from './page.css'
+import { wrapper } from './page.css';
 import { PreviousSchedule } from './previous-schedule';
 import { Recommendation } from './recommendation';
-import { Trend } from './trend'
+import { Trend } from './trend';
 import { UpcomingSchedule } from './upcoming-schedule';
 
 export default function FeedPage() {
--- services/web/src/features/Header/components/FriendProfile/FriendProfile.tsx
+++ services/web/src/features/Header/components/FriendProfile/FriendProfile.tsx
@@ -1,11 +1,9 @@
 interface FriendProfileProps {
-  isMinimize: boolean
+  isMinimize: boolean;
 }
 
 function FriendProfile({ isMinimize }: FriendProfileProps) {
-  return (
-    <div style={{ color: 'white' }}>지인페이지 프로필</div>
-  )
+  return <div style={{ color: 'white' }}>지인페이지 프로필</div>;
 }
 
-export default FriendProfile
+export default FriendProfile;
