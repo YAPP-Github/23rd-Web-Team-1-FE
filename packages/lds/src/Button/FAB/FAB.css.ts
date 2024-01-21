f2181edff08e0c50e3a1e7a8dc29ca248ba974c5
useonglee <useong0830@naver.com>
2024-01-21 20:52:04 +0900

    fix: 버튼 너비 모바일, PC 환경 대응

--- packages/lds/src/Button/FAB/FAB.css.ts
+++ packages/lds/src/Button/FAB/FAB.css.ts
@@ -4,15 +4,26 @@ import { style } from '@vanilla-extract/css';
 export const container = style({
   position: 'fixed',
   bottom: '1.6rem',
-  right: 'calc((100% - 68rem) / 2)',
+  right: '0',
   zIndex: 1000,
   margin: '0 auto',
   maxWidth: '68rem',
+  minWidth: '8.8rem',
   width: '100%',
   height: '4.8rem',
+
+  '@media': {
+    'screen and (min-width: 720px)': {
+      right: 'calc((100% - 68rem) / 2)',
+    },
+  },
 });
 
-export const buttonRecipe = style({
+export const innerContariner = style({
+  padding: '0 2rem',
+});
+
+export const button = style({
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
--- packages/lds/src/Button/FAB/FAB.tsx
+++ packages/lds/src/Button/FAB/FAB.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 import { HTMLAttributes, ReactNode } from 'react';
 import { useEffect, useState } from 'react';
 
-import { buttonRecipe, container, buttonText } from './FAB.css';
+import { button, container, buttonText, innerContariner } from './FAB.css';
 import { Txt } from '../../Txt';
 
 type ButtonType = 'extand' | 'default';
@@ -41,21 +41,23 @@ const FAB = ({ className, iconUrl = '', text, type = 'extand', ...props }: Props
     <motion.div
       className={container}
       initial={{
-        width: type === 'default' ? '4.8rem' : '100%',
+        width: type === 'default' ? '8.8rem' : '100%',
       }}
       animate={{
-        width: '4.8rem',
+        width: '8.8rem',
       }}
       transition={{ duration: 0.3, delay: 10 }}
     >
-      <button type="button" className={clsx(buttonRecipe, className)} {...props}>
-        <Image src={iconUrl} width={32} height={32} alt="" />
-        {fabType === 'extand' && (
-          <Txt typography="p3" className={buttonText}>
-            {text}
-          </Txt>
-        )}
-      </button>
+      <div className={innerContariner}>
+        <button type="button" className={clsx(button, className)} {...props}>
+          <Image src={iconUrl} width={32} height={32} alt="" />
+          {fabType === 'extand' && (
+            <Txt typography="p3" className={buttonText}>
+              {text}
+            </Txt>
+          )}
+        </button>
+      </div>
     </motion.div>
   );
 };
