ef7fca445079028dbdf09385b4461484dd899b0f
useonglee <useong0830@naver.com>
2024-01-23 03:02:22 +0900

    fix: 디자인 시스템 colors 수정

--- packages/styles/src/colors.css.ts
+++ packages/styles/src/colors.css.ts
@@ -1,13 +1,55 @@
 import { createTheme, createThemeContract } from '@vanilla-extract/css';
 
-export const baseColors = {
+const primary = {
+  blue050: '#F0F6FF',
+  blue100: '#BCD6FF',
+  blue200: '#9CC3FF',
+  blue300: '#6FA7FF',
+  blue400: '#5396FF',
+  blue500: '#287CFF',
+  blue600: '#2471E8',
+  blue700: '#1C58B5',
+  blue800: '#16448C',
+  blue900: '#11346B',
+};
+
+const secondary = {
+  green: '#58DB67',
+  lightBlue: '#111CDF2',
+  purple: '#CE7AF0',
+  magenta: '#FF70B0',
+  orange: '#FF875C',
+};
+
+const semantic = {
+  red050: '#ffeeee',
+  red100: '#ffcbcb',
+  red200: '#ffb2b2',
+  red300: '#ff8e8e',
+  red400: '#ff7979',
+  red500: '#ff5757',
+  red600: '#e84f4f',
+  red700: '#b53e3e',
+  red800: '#8c3030',
+  red900: '#6b2525',
+};
+
+const gradation = {
+  gradationBlue: 'linear-gradient(to right, #713EFF, #0989FF)',
+  gradationMagenta: 'linear-gradient(to right, #FB7A93, #FF586C);',
+};
+
+const baseColors = {
   white: '#ffffff',
   black: '#000000',
   background: '#f1f3f5',
-  primary: '#287cff',
+  ...primary,
+  ...secondary,
+  ...semantic,
+  ...gradation,
 };
 
-export const colorVariants = createThemeContract({
+const colorVariants = createThemeContract({
   colors: {
     ...baseColors,
     gray000: null,
