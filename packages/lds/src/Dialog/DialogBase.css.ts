a12b6cd95a4fe775dc9774fb2b557b84ef11deaf
useonglee <useong0830@naver.com>
2024-01-24 04:57:52 +0900

    fix: DialogBase children prop 받을 수 있도록 수정

--- packages/lds/src/Dialog/DialogBase.css.ts
+++ packages/lds/src/Dialog/DialogBase.css.ts
@@ -6,9 +6,9 @@ export const dialogOverlay = recipe({
     position: 'fixed',
     top: 0,
     left: 0,
-    right: 0,
-    bottom: 0,
-    zIndex: 100,
+    width: '100vw',
+    height: '100vh',
+    zIndex: 'auto',
   },
 
   variants: {
