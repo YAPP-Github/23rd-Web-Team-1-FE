6cf34fbb690bc4baa05035f3eb42f49bd5042357
useonglee <useong0830@naver.com>
2024-01-26 22:21:21 +0900

    feat: Profile 디자인시스템 추가

---
+++ packages/lds/src/Profile/Profile.stories.tsx
@@ -0,0 +1,33 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import Profile from './Profile';
+import { Spacing } from '../Spacing';
+
+const meta: Meta<typeof Profile> = {
+  title: 'Profile',
+  component: Profile,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Profile>;
+
+export const Template: Story = {
+  render: () => {
+    return (
+      <>
+        {/* 기본 이미지 */}
+        <Profile />
+
+        <Spacing size={16} />
+
+        {/* 샘플 이미지 */}
+        <Profile
+          profileImgUrl="https://static.im-linker.com/images/profile-sample.png"
+          alt="샘플 이미지"
+        />
+      </>
+    );
+  },
+};
