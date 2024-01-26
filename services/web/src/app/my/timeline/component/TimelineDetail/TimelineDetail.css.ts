961441cd12844793f8c3409bc02151689713ca5b
JjungminLee <julie0964@naver.com>
2024-01-27 07:38:07 +0900

    feat: Timeline상세페이지 틀 구성

---
+++ services/web/src/app/my/timeline/component/TimelineDetail/TimelineDetail.css.ts
@@ -0,0 +1,7 @@
+import { style } from '@vanilla-extract/css';
+
+export const TimelineDetaileHeaderWrapper = style({});
+export const TimelineDetailTitleWrapper = style({});
+export const TimelineDetailCalendarWrapper = style({});
+export const TimelineDetailInvitationWrapper = style({});
+export const TimelineDetailNoteWrapper = style({});
---
+++ services/web/src/app/my/timeline/component/TimelineDetail/TimelineDetail.tsx
@@ -0,0 +1,17 @@
+import { List } from '@linker/lds';
+
+export const TimelineDetail = () => {
+  return (
+    <>
+      <List>
+        <h1>Calendar</h1>
+      </List>
+      <List>
+        <h1>Invitation</h1>
+      </List>
+      <List>
+        <h1>Note</h1>
+      </List>
+    </>
+  );
+};
