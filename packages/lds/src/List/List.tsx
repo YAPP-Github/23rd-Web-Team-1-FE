2d47050b37183dc25997078ddc18d1517c82659b
useonglee <useong0830@naver.com>
2024-02-04 04:21:27 +0900

    fix: List 컴포넌트 div attribute props 받을 수 있도록 prop 수정

--- packages/lds/src/List/List.tsx
+++ packages/lds/src/List/List.tsx
@@ -5,19 +5,21 @@ import { listContainer, listContent } from './List.css';
 import ListHeader from './ListHeader';
 import ListRow from './ListRow';
 
-interface ListProps {
+interface ListProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
   className?: string;
 }
 
-const List = forwardRef<HTMLDivElement, ListProps>(({ children, className }: ListProps, ref) => {
-  return (
-    <section className={listContainer}>
-      <div ref={ref} className={clsx(listContent, className)}>
-        {children}
-      </div>
-    </section>
-  );
-});
+const List = forwardRef<HTMLDivElement, ListProps>(
+  ({ children, className, ...props }: ListProps, ref) => {
+    return (
+      <section className={listContainer} {...props}>
+        <div ref={ref} className={clsx(listContent, className)}>
+          {children}
+        </div>
+      </section>
+    );
+  },
+);
 
 export default Object.assign(List, { Header: ListHeader, Row: ListRow });
