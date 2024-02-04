c0fe54d582bb57121e1cfc3a32feec26caa8bbd0
useonglee <useong0830@naver.com>
2024-02-04 17:40:22 +0900

    fix: next config image domains 제거 및 remotePatterns 으로 대체

--- services/web/next.config.js
+++ services/web/next.config.js
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-var-requires, require-await */
 /** @type {import('next').NextConfig} */
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
@@ -9,13 +9,38 @@ const withVanillaExtract = createVanillaExtractPlugin({ identifiers: 'short' });
 
 const nextConfig = {
   images: {
-    domains: [
-      'static.im-linker.com',
-      'newsimg.sedaily.com',
-      'k.kakaocdn.net',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static.im-linker.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'k.kakaocdn.net',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'k.kakaocdn.net',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'newsimg.sedaily.com',
+        pathname: '/**',
+      },
       /** @todo 서버에서 mock데이터로 해당 도메인을 내려줘서 임시로 추가. 추후에 삭제 필요 */
-      'postfiles.pstatic.net',
-      'r.yna.co.kr',
+      {
+        protocol: 'https',
+        hostname: 'postfiles.pstatic.net',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'r.yna.co.kr',
+        pathname: '/**',
+      },
     ],
     formats: ['image/webp'],
     dangerouslyAllowSVG: true,
