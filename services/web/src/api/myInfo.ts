e29e7d33e79b209c53da26c126fded75aa6b933d
useonglee <useong0830@naver.com>
2024-02-06 02:04:09 +0900

    feat: getMyInfoWithServer API 추가

---
+++ services/web/src/api/myInfo.ts
@@ -0,0 +1,14 @@
+import { MyInfo } from '@/types/myInfo';
+import { ky } from '@linker/ky';
+
+import { getTokens } from '@utils/token/server';
+
+export const getMyInfoWithServer = () => {
+  if (getTokens().accessToken == null) {
+    return null;
+  }
+
+  const data = ky.get<MyInfo>('/v1/my');
+
+  return data;
+};
