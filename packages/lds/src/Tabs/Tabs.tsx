0e8a5dd3077ff22aff9578fafda493264725c0f8
useonglee <useong0830@naver.com>
2024-02-06 02:02:48 +0900

    fix: Tab 컴포넌트 구조 임시 수정

--- packages/lds/src/Tabs/Tabs.tsx
+++ packages/lds/src/Tabs/Tabs.tsx
@@ -1,68 +1,43 @@
-'use client'
+'use client';
 
-import { clsx } from 'clsx'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
+import { clsx } from 'clsx';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
-import {
-  tabs,
-  tapList,
-  tapItem,
-  link,
-  activeLink,
-  bottomLine,
-  activeLine,
-} from './Tabs.css'
+import { tabs, tapList, tapItem, link, activeLink, bottomLine, activeLine } from './Tabs.css';
 
 export interface TabInfo {
-  href: string
-  text: string
-  textSpan: HTMLSpanElement | null
+  href: string;
+  text: string;
+  textSpan: HTMLSpanElement | null;
 }
 
 interface TabsProps {
-  tabInfos: React.MutableRefObject<TabInfo[]>
+  tabInfos: TabInfo[];
 }
 
 function Tabs({ tabInfos }: TabsProps) {
-  const pathname = usePathname()
-  const [activeTabStyle, setActiveTabStyle] = useState<React.CSSProperties>({})
-
-  useEffect(() => {
-    const matchedRoute = tabInfos.current.find(tabInfo => tabInfo.href === pathname)
-
-    if (matchedRoute) {
-      const { textSpan } = matchedRoute
-
-      if (textSpan) {
-        setActiveTabStyle({
-          width: `${textSpan.offsetWidth}px`,
-          left: `${textSpan.offsetLeft}px`,
-        })
-      }
-    }
-  }, [pathname, tabInfos])
+  const pathname = usePathname();
 
   return (
     <nav className={tabs}>
       <ul className={tapList}>
-        {tabInfos.current.map(tabInfo => (
+        {tabInfos.map((tabInfo) => (
           <li className={tapItem} key={tabInfo.href}>
             <Link
               href={tabInfo.href}
               className={clsx(link, pathname === tabInfo.href && activeLink)}
             >
-              <span ref={ref => tabInfo.textSpan = ref}>{tabInfo.text}</span>
+              <span ref={(ref) => (tabInfo.textSpan = ref)}>{tabInfo.text}</span>
             </Link>
           </li>
         ))}
       </ul>
       <div className={bottomLine}>
-        <div className={activeLine} style={activeTabStyle} />
+        <div className={activeLine} />
       </div>
     </nav>
-  )
+  );
 }
 
-export default Tabs
+export default Tabs;
