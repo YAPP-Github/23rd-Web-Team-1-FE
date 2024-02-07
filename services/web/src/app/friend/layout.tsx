032b76c80803999ceed428fff14220c24de0cf5d
useonglee <useong0830@naver.com>
2024-02-08 06:14:27 +0900

    fix: Friend Header 임시 제거

--- services/web/src/app/friend/layout.tsx
+++ services/web/src/app/friend/layout.tsx
@@ -1,10 +1,8 @@
-import { FriendHeader } from '@features/Header';
 import { Layout } from '@linker/lds';
 
 function FriendLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <FriendHeader />
       <Layout>{children}</Layout>
     </>
   );
