d666f2b5763e57d1c1decb1a2964192e02efbeae
useonglee <useong0830@naver.com>
2024-02-08 06:13:05 +0900

    fix: Icon 컴포넌트 height prop 추가

--- packages/lds/src/Icon/Icon.tsx
+++ packages/lds/src/Icon/Icon.tsx
@@ -5,15 +5,16 @@ const bucket = 'https://static.im-linker.com';
 interface Props extends Omit<ImageProps, 'src' | 'alt' | 'width' | 'height'> {
   name: string;
   size?: number;
+  height?: number;
 }
 
-const Icon = ({ name, size = 32, ...props }: Props) => {
+const Icon = ({ name, size = 32, height, ...props }: Props) => {
   return (
     <Image
       src={`${bucket}/icons/${name}.svg`}
       alt={`${name} icon`}
       width={size}
-      height={size}
+      height={height ?? size}
       {...props}
     />
   );
