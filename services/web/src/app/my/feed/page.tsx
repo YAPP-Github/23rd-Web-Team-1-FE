f8927056af679fe85373e551cb3a93f6e4048ac6
useonglee <useong0830@naver.com>
2024-02-22 19:46:10 +0900

    fix: 로그인 배너랑 UpcomingSchedule 컴포넌트 분리

--- services/web/src/app/my/feed/page.tsx
+++ services/web/src/app/my/feed/page.tsx
@@ -1,3 +1,6 @@
+import { getTokens } from '@utils/token/server';
+
+import { ContactBanner } from './contact-banner';
 import { wrapper } from './page.css';
 import { PreviousSchedule } from './previous-schedule';
 import { Recommendation } from './recommendation';
@@ -5,8 +8,11 @@ import { Trend } from './trend';
 import { UpcomingSchedule } from './upcoming-schedule';
 
 export default function FeedPage() {
+  const { accessToken } = getTokens();
+
   return (
     <article className={wrapper}>
+      {accessToken != null && <ContactBanner />}
       <UpcomingSchedule />
       <Recommendation />
       <PreviousSchedule />
