624cde22536e6a544809445919b1692f1c00ee84
useonglee <useong0830@naver.com>
2024-02-22 19:46:40 +0900

    fix: 로그인 모달 스타일 추가

--- packages/lds/src/Logo/Logo.tsx
+++ packages/lds/src/Logo/Logo.tsx
@@ -7,10 +7,12 @@ interface Props {
 }
 
 const Logo = ({ type = 'default', width = 72, height = 24 }: Props) => {
+  const logoName = type === 'default' ? 'logo' : 'logo-fill';
+
   return (
     <Image
-      src={`https://static.im-linker.com/icons/${type === 'default' ? 'logo' : 'logo-fill'}.svg`}
-      blurDataURL={`https://static.im-linker.com/icons/${type === 'default' ? 'logo' : 'logo-fill'}.svg`}
+      src={`https://static.im-linker.com/icons/${logoName}.svg`}
+      blurDataURL={`https://static.im-linker.com/icons/${logoName}.svg`}
       alt="Linker logo"
       width={width}
       height={height}
