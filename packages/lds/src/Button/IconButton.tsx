c82ba3120ad02792afb765a5c63c9e34db38cb6e
useonglee <useong0830@naver.com>
2024-02-23 17:06:45 +0900

    fix: IconButton size prop 추가

--- packages/lds/src/Button/IconButton.tsx
+++ packages/lds/src/Button/IconButton.tsx
@@ -4,13 +4,14 @@ import { Icon } from '../Icon';
 
 interface Props extends HTMLAttributes<HTMLButtonElement> {
   name: string;
+  size?: number;
   className?: string;
 }
 
-const IconButton = ({ name, className, ...props }: Props) => {
+const IconButton = ({ name, size = 32, className, ...props }: Props) => {
   return (
     <button type="button" className={className} {...props}>
-      <Icon name={name} />
+      <Icon name={name} size={size} />
     </button>
   );
 };
