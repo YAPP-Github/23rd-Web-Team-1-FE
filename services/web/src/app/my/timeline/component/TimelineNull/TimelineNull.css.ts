72ebbdd6842241e7508ef5e20859670624e430c6
JungminLee <85864699+JjungminLee@users.noreply.github.com>
2024-02-23 21:39:16 +0900

    feat: [LINKER-135] 캘린더에 데이터가 없을때 에러페이지 (#70)

---
+++ services/web/src/app/my/timeline/component/TimelineNull/TimelineNull.css.ts
@@ -0,0 +1,16 @@
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  display: 'flex',
+  width: '100%',
+  height: '40rem',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
+export const wrapper = style({
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
