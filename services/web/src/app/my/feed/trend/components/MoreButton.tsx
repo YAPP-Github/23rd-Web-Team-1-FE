214bffbf4faf0bb25d7ab71c5887a129aa4f0074
danivelop <mofg266@gmail.com>
2024-02-23 22:58:01 +0900

    fix: 트렌드 핫이슈 카드의 more button에 링크 연결

--- services/web/src/app/my/feed/trend/components/MoreButton.tsx
+++ services/web/src/app/my/feed/trend/components/MoreButton.tsx
@@ -1,29 +1,32 @@
 import { Button, Txt } from '@linker/lds';
 import { colors } from '@linker/styles';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { button } from './MoreButton.css';
 
 function MoreButton() {
   return (
-    <Button className={button}>
-      <Image
-        src="https://static.im-linker.com/icons/refresh.png"
-        width={20}
-        height={20}
-        alt="pencil-icon"
-      />
-      <Txt typography="b3" color={colors.blue500}>
-        관심사 최신뉴스&nbsp;
-      </Txt>
-      <Txt typography="b3" color={colors.gray500}>
-        더 보기&nbsp;
-      </Txt>
-      {/** @todo 기획적으로 명확하지 않아 임시로 markup만 작성. 추후 구현필요 */}
-      <Txt typography="b3" color={colors.gray500}>
-        1/5
-      </Txt>
-    </Button>
+    <Link href="/trend">
+      <Button className={button}>
+        <Image
+          src="https://static.im-linker.com/icons/refresh.png"
+          width={20}
+          height={20}
+          alt="pencil-icon"
+        />
+        <Txt typography="b3" color={colors.blue500}>
+          관심사 최신뉴스&nbsp;
+        </Txt>
+        <Txt typography="b3" color={colors.gray500}>
+          더 보기&nbsp;
+        </Txt>
+        {/** @todo 기획적으로 명확하지 않아 임시로 markup만 작성. 추후 구현필요 */}
+        <Txt typography="b3" color={colors.gray500}>
+          1/5
+        </Txt>
+      </Button>
+    </Link>
   );
 }
 
